0ca14f30aeffb805dc2b3898f144d3f373700e72
KtaVC11 <kataviqc@hotmail.com>
2020-09-04 17:26:33 -0600

    some init msg

---
+++ my-gatsby-project/gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: [
+    // Tell gatsby which theme you will be using.
+    {
+      resolve: "gatsby-wordpress-theme-phoenix",
+      options: {
+        wordPressUrl: 'http://localhost/wordpressGatsby'
+      }
+    } ]
+};
